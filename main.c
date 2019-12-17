4426ebe6174a11c4bb55550e14b0ca6ec51450ca
Nananinana <nina.b746@gmail.com>
2019-12-17 16:49:37 +0100

    cokolwiek

--- main.c
+++ main.c
@@ -6,42 +6,42 @@
   ******************************************************************************
   * This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
+  * USER CODE END. Other portions of this file, whether
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
-  * Copyright (c) 2018 STMicroelectronics International N.V. 
+  * Copyright (c) 2018 STMicroelectronics International N.V.
   * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without 
+  * Redistribution and use in source and binary forms, with or without
   * modification, are permitt
   ed, provided that the following conditions are met:
   *
-  * 1. Redistribution of source code must retain the above copyright notice, 
+  * 1. Redistribution of source code must retain the above copyright notice,
   *    this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright notice,
   *    this list of conditions and the following disclaimer in the documentation
   *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
   *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
+  * 4. This software, including modifications and/or derivative works of this
   *    software, must execute solely and exclusively on microcontroller or
   *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
   *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
   * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
   * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
   * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
   * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
@@ -49,6 +49,7 @@
   */
 
 #include "main.h"
+#include "bam_glo.h"
 
 int __io_putchar(int ch) {
     uint8_t data = ch;
@@ -159,7 +160,8 @@ int main(void) {
 
     osMessageQDef(audioRequestsQueue, AUDIO_QUEUE_SIZE, AudioRequest);
     audioRequestsQueue = osMessageCreate(osMessageQ(audioRequestsQueue), NULL);
-
+    bam_reset(NULL, NULL);
+    struct bam_static_param lol;
     /* Create the thread(s) */
     osThreadDef(defaultTask, StartDefaultTask, osPriorityLow, 1, ALL_THREADS_STACK_SIZE * 0.1);
     osThreadDef(audioPlayerTask, StartAudioPlayerTask, osPriorityLow, 1, ALL_THREADS_STACK_SIZE * 0.1);
@@ -213,7 +215,7 @@ void StartAudioPlayerTask(void const *argument) {
     osEvent event;
 
     for (;;) {
-        if (!APP_STATE.IS_PLAYING) {		
+        if (!APP_STATE.IS_PLAYING) {
             event = osMessageGet(audioRequestsQueue, osWaitForever);
             if (event.status == osEventMessage) {
                 request = event.value.p;
@@ -235,7 +237,7 @@ void StartTouchscreenTask(void const *argument) {
         BSP_TS_GetState(&TS_State);
         if (TS_State.touchDetected > 0) {
             APP_STATE.IS_DIRTY = 1;
-            GUI_HandleTouch(&TS_State, CON_HandleButtonTouched);	
+            GUI_HandleTouch(&TS_State, CON_HandleButtonTouched);
         }
         vTaskDelay(100);
     }
@@ -245,13 +247,13 @@ void StartGuiTask(void const *argument) {
     for (;;) {
         if (APP_STATE.IS_DIRTY == 1) {
             GUI_DrawAllButtons();
-			
+
 			if (APP_STATE.SELECTED_OPTION != -1)
 				GUI_HighlightButton(APP_STATE.SELECTED_OPTION);
 
 			if (APP_STATE.SELECTED_SOUND_BUTTON != -1)
 				GUI_HighlightButton(APP_STATE.SELECTED_SOUND_BUTTON);
-			
+
 			APP_STATE.IS_DIRTY = -1;
         }
 		vTaskDelay(100);
@@ -264,13 +266,13 @@ void SystemClock_Config(void) {
     RCC_ClkInitTypeDef RCC_ClkInitStruct;
     RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
-    /**Configure the main internal regulator output voltage 
+    /**Configure the main internal regulator output voltage
     */
     __HAL_RCC_PWR_CLK_ENABLE();
 
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
@@ -285,13 +287,13 @@ void SystemClock_Config(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Activate the Over-Drive mode 
+    /**Activate the Over-Drive mode
     */
     if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
                                   | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
@@ -332,11 +334,11 @@ void SystemClock_Config(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Configure the Systick interrupt time 
+    /**Configure the Systick interrupt time
     */
     HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
-    /**Configure the Systick 
+    /**Configure the Systick
     */
     HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -349,7 +351,7 @@ static void MX_ADC3_Init(void) {
 
     ADC_ChannelConfTypeDef sConfig;
 
-    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
     */
     hadc3.Instance = ADC3;
     hadc3.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
@@ -367,7 +369,7 @@ static void MX_ADC3_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
     */
     sConfig.Channel = ADC_CHANNEL_4;
     sConfig.Rank = ADC_REGULAR_RANK_1;
@@ -451,13 +453,13 @@ static void MX_I2C1_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Configure Analogue filter 
+    /**Configure Analogue filter
     */
     if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Configure Digital filter 
+    /**Configure Digital filter
     */
     if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK) {
         _Error_Handler(__FILE__, __LINE__);
@@ -481,13 +483,13 @@ static void MX_I2C3_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Configure Analogue filter 
+    /**Configure Analogue filter
     */
     if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Configure Digital filter 
+    /**Configure Digital filter
     */
     if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK) {
         _Error_Handler(__FILE__, __LINE__);
@@ -575,7 +577,7 @@ static void MX_RTC_Init(void) {
 
     /* USER CODE END RTC_Init 1 */
 
-    /**Initialize RTC Only 
+    /**Initialize RTC Only
     */
     hrtc.Instance = RTC;
     hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
@@ -588,7 +590,7 @@ static void MX_RTC_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Initialize RTC and set the Time and Date 
+    /**Initialize RTC and set the Time and Date
     */
     sTime.Hours = 0x0;
     sTime.Minutes = 0x0;
@@ -608,7 +610,7 @@ static void MX_RTC_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Enable the Alarm A 
+    /**Enable the Alarm A
     */
     sAlarm.AlarmTime.Hours = 0x0;
     sAlarm.AlarmTime.Minutes = 0x0;
@@ -625,7 +627,7 @@ static void MX_RTC_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Enable the Alarm B 
+    /**Enable the Alarm B
     */
     sAlarm.AlarmDateWeekDay = 0x1;
     sAlarm.Alarm = RTC_ALARM_B;
@@ -633,7 +635,7 @@ static void MX_RTC_Init(void) {
         _Error_Handler(__FILE__, __LINE__);
     }
 
-    /**Enable the TimeStamp 
+    /**Enable the TimeStamp
     */
     if (HAL_RTCEx_SetTimeStamp(&hrtc, RTC_TIMESTAMPEDGE_RISING, RTC_TIMESTAMPPIN_POS1) != HAL_OK) {
         _Error_Handler(__FILE__, __LINE__);
@@ -1105,9 +1107,9 @@ static void MX_FMC_Init(void) {
 
 }
 
-/** Configure pins as 
-        * Analog 
-        * Input 
+/** Configure pins as
+        * Analog
+        * Input
         * Output
         * EVENT_OUT
         * EXTI
@@ -1322,7 +1324,7 @@ void _Error_Handler(char *file, int line) {
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
